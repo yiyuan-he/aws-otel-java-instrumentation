9a6a475ecbaa73ffd249a3eaa7507425d30c57e7
Michael He <yiyuanh@proton.me>
2024-10-11 22:05:57 +0000

    testing: commenting out all contract tests besides bedrock

--- appsignals-tests/contract-tests/src/test/java/software/amazon/opentelemetry/appsignals/test/awssdk/v1/AwsSdkV1Test.java
+++ appsignals-tests/contract-tests/src/test/java/software/amazon/opentelemetry/appsignals/test/awssdk/v1/AwsSdkV1Test.java
@@ -15,14 +15,10 @@
 
 package software.amazon.opentelemetry.appsignals.test.awssdk.v1;
 
-import io.opentelemetry.proto.common.v1.KeyValue;
-import java.util.List;
-import org.assertj.core.api.ThrowingConsumer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import software.amazon.opentelemetry.appsignals.test.awssdk.base.AwsSdkBaseTest;
-import software.amazon.opentelemetry.appsignals.test.utils.SemanticConventionsConstants;
 
 @Testcontainers(disabledWithoutDocker = true)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -110,96 +106,96 @@ public class AwsSdkV1Test extends AwsSdkBaseTest {
     return "AWSBedrockAgentRuntime";
   }
 
-  @Test
-  void testS3CreateBucket() throws Exception {
-    doTestS3CreateBucket();
-  }
-
-  @Test
-  void testS3CreateObject() throws Exception {
-    doTestS3CreateObject();
-  }
-
-  @Test
-  void testS3GetObject() throws Exception {
-    doTestS3GetObject();
-  }
-
-  @Test
-  void testS3Error() {
-    doTestS3Error();
-  }
-
-  @Test
-  void testS3Fault() {
-    doTestS3Fault();
-  }
-
-  @Override
-  protected List<ThrowingConsumer<KeyValue>> dynamoDbAttributes(
-      String operation, String tableName) {
-    return List.of(assertAttribute(SemanticConventionsConstants.AWS_TABLE_NAME, tableName));
-  }
-
-  @Test
-  void testDynamoDbCreateTable() {
-    doTestDynamoDbCreateTable();
-  }
-
-  @Test
-  void testDynamoDbPutItem() {
-    doTestDynamoDbPutItem();
-  }
-
-  @Test
-  void testDynamoDbError() throws Exception {
-    doTestDynamoDbError();
-  }
-
-  @Test
-  void testDynamoDbFault() throws Exception {
-    doTestDynamoDbFault();
-  }
-
-  @Test
-  void testSQSCreateQueue() throws Exception {
-    doTestSQSCreateQueue();
-  }
-
-  @Test
-  void testSQSSendMessage() throws Exception {
-    doTestSQSSendMessage();
-  }
-
-  @Test
-  void testSQSReceiveMessage() throws Exception {
-    doTestSQSReceiveMessage();
-  }
-
-  @Test
-  void testSQSError() throws Exception {
-    doTestSQSError();
-  }
-
-  @Test
-  void testSQSFault() throws Exception {
-    doTestSQSFault();
-  }
-
-  @Test
-  void testKinesisPutRecord() throws Exception {
-    doTestKinesisPutRecord();
-  }
-
-  @Test
-  void testKinsesisError() throws Exception {
-    doTestKinesisError();
-  }
-
-  @Test
-  void testKinesisFault() throws Exception {
-    doTestKinesisFault();
-  }
+  //@Test
+  // void testS3CreateBucket() throws Exception {
+  //  doTestS3CreateBucket();
+  // }
+  //
+  // @Test
+  // void testS3CreateObject() throws Exception {
+  //  doTestS3CreateObject();
+  // }
+  //
+  // @Test
+  // void testS3GetObject() throws Exception {
+  //  doTestS3GetObject();
+  // }
+  //
+  // @Test
+  // void testS3Error() {
+  //  doTestS3Error();
+  // }
+  //
+  // @Test
+  // void testS3Fault() {
+  //  doTestS3Fault();
+  // }
+  //
+  // @Override
+  // protected List<ThrowingConsumer<KeyValue>> dynamoDbAttributes(
+  //    String operation, String tableName) {
+  //  return List.of(assertAttribute(SemanticConventionsConstants.AWS_TABLE_NAME, tableName));
+  // }
+  //
+  // @Test
+  // void testDynamoDbCreateTable() {
+  //  doTestDynamoDbCreateTable();
+  // }
+  //
+  // @Test
+  // void testDynamoDbPutItem() {
+  //  doTestDynamoDbPutItem();
+  // }
+  //
+  // @Test
+  // void testDynamoDbError() throws Exception {
+  //  doTestDynamoDbError();
+  // }
+  //
+  // @Test
+  // void testDynamoDbFault() throws Exception {
+  //  doTestDynamoDbFault();
+  // }
+  //
+  // @Test
+  // void testSQSCreateQueue() throws Exception {
+  //  doTestSQSCreateQueue();
+  // }
+  //
+  // @Test
+  // void testSQSSendMessage() throws Exception {
+  //  doTestSQSSendMessage();
+  // }
+  //
+  // @Test
+  // void testSQSReceiveMessage() throws Exception {
+  //  doTestSQSReceiveMessage();
+  // }
+  //
+  // @Test
+  // void testSQSError() throws Exception {
+  //  doTestSQSError();
+  // }
+  //
+  // @Test
+  // void testSQSFault() throws Exception {
+  //  doTestSQSFault();
+  // }
+  //
+  // @Test
+  // void testKinesisPutRecord() throws Exception {
+  //  doTestKinesisPutRecord();
+  // }
+  //
+  // @Test
+  // void testKinsesisError() throws Exception {
+  //  doTestKinesisError();
+  // }
+  //
+  // @Test
+  // void testKinesisFault() throws Exception {
+  //  doTestKinesisFault();
+  // }
 
   @Test
   void testBedrockAgentGetKnowledgeBaseId() {
